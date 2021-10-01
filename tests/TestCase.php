a9e700739f9048d2395c0661f50000ceb9c597ef
Claudio Ludovico Panetta <745074-ludo237@users.noreply.gitlab.com>
2021-10-01 15:26:14 +0000

    Add base files

---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ludo237\Rules\Tests;
+
+
+/**
+ * TestCase
+ * @package Ludo237\DelayedArtisticGuppy\Tests
+ */
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+}
